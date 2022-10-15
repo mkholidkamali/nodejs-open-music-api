0e60daf7f4d4074e425dd5be4d5b3fb45426ebbd
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-10-15 21:07:46 +0700

    [New] Album Service

--- src/services/postgres/albumService.js
+++ src/services/postgres/albumService.js
@@ -31,6 +31,48 @@ class AlbumService {
         const result = this.__pool.query('SELECT * FROM album');
         return result.rows.map(mapAlbumDBToModel);
     }
+
+    async getAlbumById(id) {
+        const query = {
+            text: "SELECT * FROM album WHERE id = $1",
+            values: [id]
+        };
+
+        const result = await this.__pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album tidak ditemukan');
+        }
+
+        return result.rows.map(mapAlbumDBToModel)[0];
+    }
+
+    async editAlbumById(id, { name, year }) {
+        const updatedAt = new Date().toISOString;
+        const query = {
+            text: "UPDATE album SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id",
+            values: [name, year, updatedAt, id]
+        };
+
+        const result = await this.__pool(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan')
+        }
+    }
+
+    async deleteAlbumById(id) {
+        const query = {
+            text: "DELETE FROM album WHERE id = $1 RETURNING id",
+            values: [id]
+        }
+
+        const result = await this.__pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan')
+        }
+    }
 }
 
 module.exports = AlbumService;
