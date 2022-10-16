f48a3598ba028330afdf9c5ca2399909a4688f70
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-10-16 14:18:46 +0700

    [Fix] Album - Service Pool

--- src/services/postgres/albumService.js
+++ src/services/postgres/albumService.js
@@ -49,13 +49,13 @@ class AlbumService {
     }
 
     async editAlbumById(id, { name, year }) {
-        const updatedAt = new Date().toISOString;
+        const updatedAt = new Date().toISOString();
         const query = {
             text: "UPDATE album SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id",
             values: [name, year, updatedAt, id]
         };
 
-        const result = await this.__pool(query);
+        const result = await this.__pool.query(query);
 
         if (!result.rows.length) {
             throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan')
