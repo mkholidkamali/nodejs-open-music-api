20fd4a580856a803f824194db31db2fda21d3018
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-10-16 09:49:31 +0700

    [New] Error Handling

--- src/server.js
+++ src/server.js
@@ -6,11 +6,13 @@ const SongsService = require('./services/postgres/songsService');
 const album = require('./api/album');
 const songs = require('./api/songs');
 const { AlbumValidator } = require('../validator/album');
+const ClientError = require('./exception/ClientError');
 
 const init = async () => {
     const albumService = new AlbumService();
     const songsService = new SongsService();
 
+    // Server Configuration
     const server = Hapi.server({
         port: process.env.PORT,
         host: process.env.HOST,
@@ -21,6 +23,7 @@ const init = async () => {
         }
     });
 
+    // Plugin
     await server.register([
         {
             plugin: album,
@@ -37,6 +40,40 @@ const init = async () => {
             }
         }
     ]);
+
+    // Error Handling
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+
+        // If Error
+        if (response instanceof Error) {
+            // Internal Error
+            if (response instanceof ClientError) {
+                const newResponse = h.response({
+                    status: 'fail',
+                    message: response.message
+                });
+                newResponse.code(response.statusCode);
+                return newResponse;
+            }
+        
+            // Native Error
+            if (!response.isServer) {
+                return h.continue;
+            }
+
+            // Server Error
+            const newResponse = h.response({
+                status: 'error',
+                message: 'terjadi kegagalan pada server kami'
+            });
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        // If OK
+        return h.continue;
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
